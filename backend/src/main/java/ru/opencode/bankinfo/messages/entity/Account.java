8eb37df157c5ed17b1aa452380664b20c2042a39
Andrey Bartenev <59123372+MrRobinGoood@users.noreply.github.com>
2023-07-09 15:28:44 +0400

    Fix "notBlank"

--- backend/src/main/java/ru/opencode/bankinfo/messages/entity/Account.java
+++ backend/src/main/java/ru/opencode/bankinfo/messages/entity/Account.java
@@ -1,6 +1,9 @@
 package ru.opencode.bankinfo.messages.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import ru.opencode.bankinfo.util.Audit;
 import ru.opencode.bankinfo.messages.entity.subClass.RstrList;
@@ -15,53 +18,41 @@ import java.time.LocalDate;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @ToString
 public class Account extends Audit {
-
-    public Account(Entry entry, String account, String regulationAccountType, Long accountCBRBIC, LocalDate dateIn) {
-        this.entry = entry;
-        this.account = account;
-        this.regulationAccountType = regulationAccountType;
-        this.accountCBRBIC = accountCBRBIC;
-        this.dateIn = dateIn;
-    }
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @EqualsAndHashCode.Include
     @Setter(AccessLevel.PRIVATE)
     private Long id;
 
-    //    @NonNull
-//    @NotNull
-//    @JsonIgnore
-//    @ToString.Exclude
-    @ToString.Exclude
+    @NonNull
+    @NotNull
+    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "entry_id", nullable = false)
     private Entry entry;
 
-    //    @NonNull
-//    @NotNull
-//    @NotBlank
-//    @Column(nullable = false)
+    @NonNull
+    @NotNull
+    @NotBlank
+    @Column(nullable = false)
     private String account;
 
-    //    @NonNull
-//    @NotNull
-//    @NotBlank
-//    @Column(nullable = false)
+    @NonNull
+    @NotNull
+    @NotBlank
+    @Column(nullable = false)
     private String regulationAccountType;
 
     private Byte controlKey;
 
-    //    @NonNull
-//    @NotNull
-//    @NotBlank
-//    @Column(nullable = false)
+    @NonNull
+    @NotNull
+    @Column(nullable = false)
     private Long accountCBRBIC;
 
-    //    @NonNull
-//    @NotNull
-//    @Column(nullable = false)
+    @NonNull
+    @NotNull
+    @Column(nullable = false)
     private LocalDate dateIn;
 
     private LocalDate dateOut;
